375cd6cfdd4d38de882acdf2232430925eb64803
olivier <olivier.dogemo@gmail.com>
2019-10-02 23:26:23 +0200

    todoList avec Ngrx architecture de type redux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from './todo.model';
+import { TodoService } from './todo.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+ public todos$: Observable<Todo[]> = this.todoService.todos$.asObservable();
+ public message: string;
+
+ constructor(private todoService: TodoService) {}
+
+ public addTodo() {
+   this.todoService.addTodo({ message: this.message, done: false });
+ }
+
+ public toggleTodo(index: number) {
+   this.todoService.toggleTodo(index);
+ }
+
+ public deleteTodo(index: number) {
+   console.log(index);
+   this.todoService.deleteTodo(index);
+ }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { TodoService } from './todo.service';
+import { FormsModule } from "@angular/forms";
+import { StoreModule } from "@ngrx/store";
+import { reducers } from './store/app.reducer';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, 
+    FlexLayoutModule,
+    StoreModule.forRoot(reducers )
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
